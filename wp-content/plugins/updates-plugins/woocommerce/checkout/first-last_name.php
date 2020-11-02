b40f50885442e6a21ede98b3812cedd1bb38be62
Dalia <dalia.easa98@eng-st.cu.edu.eg>
2020-11-02 04:28:38 +0200

    update merge first-last name

--- wp-content/plugins/updates-plugins/woocommerce/checkout/first-last_name.php
+++ wp-content/plugins/updates-plugins/woocommerce/checkout/first-last_name.php
@@ -11,10 +11,10 @@ function ced_remove_billing_fields( $fields ) {
 add_filter( 'woocommerce_checkout_fields' , 'ced_rename_checkout_fields' );
 // Change placeholder and label text
 function ced_rename_checkout_fields( $fields ) {
-$fields['billing']['billing_first_name']['placeholder'] = 'الإسم';
-$fields['billing']['billing_first_name']['label'] = 'Name';
-$fields['shipping']['shipping_first_name']['placeholder'] = 'الإسم';
-$fields['shipping']['shipping_first_name']['label'] = 'Name';
+$fields['billing']['billing_first_name']['placeholder'] = 'Name';
+$fields['billing']['billing_first_name']['label'] = 'الإسم';
+$fields['shipping']['shipping_first_name']['placeholder'] = 'Name';
+$fields['shipping']['shipping_first_name']['label'] = 'الإسم';
 return $fields;
 }
 
